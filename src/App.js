eb183ef272c2119f8d4d40556b9cbb27ca733a07
mikelovelace <mike@mlovelace.net>
2021-03-30 22:41:44 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Photo from './components/Photo'
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Photo />
     </div>
   );
 }
